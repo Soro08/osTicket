c1946dd5d74ec64d95106b9d0327e826d40a420a
Soro08 <soro4827@gmail.com>
2020-03-12 00:43:52 +0000

    Initial commit

---
+++ upload/setup/inc/footer.inc.php
@@ -0,0 +1,12 @@
+
+            <div class="clear"></div>
+        </div> <!-- content -->
+    </div> <!-- wizard -->
+    <div id="footer" class="centered">Copyright &copy; <?php echo date('Y'); ?> <a target="_blank" href="https://osticket.com">osTicket.com</a></div>
+
+    <script type="text/javascript" src="../js/jquery-3.4.0.min.js?f1e9e88"></script>
+    <script type="text/javascript" src="../js/jstz.min.js?f1e9e88"></script>
+    <script type="text/javascript" src="js/setup.js?f1e9e88"></script>
+    <script type="text/javascript" src="js/tips.js?f1e9e88"></script>
+</body>
+</html>
